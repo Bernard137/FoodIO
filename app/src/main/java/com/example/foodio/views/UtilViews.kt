5460757ecb4d91db78b59fddc9ebb10a00bfe4af
Bernard137 <bbarisic@etfos.hr>
2024-09-15 18:29:51 +0200

    -add previewButton

--- app/src/main/java/com/example/foodio/views/UtilViews.kt
+++ app/src/main/java/com/example/foodio/views/UtilViews.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.foodio.MainActivity
 import com.example.foodio.data.ShoppingCartRepository
+import com.example.foodio.ui.theme.FoodIOTheme
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @Composable
@@ -37,4 +40,12 @@ fun goBackToCleanMainActivity(context: Context) {
         .putExtra("username", ShoppingCartRepository.getUsername())
         .putExtra("first_shopping", isFirstShopping))
     (context as Activity).finish()
+}
+
+@Preview
+@Composable
+fun PreviewButton (){
+    FoodIOTheme(dynamicColor = false) {
+        MyBackButton(LocalContext.current)
+    }
 }
